91fdecc916fa7c1b8d003157f2001559d978ac2f
Riddhesh1009 <74883681+Riddhesh1009@users.noreply.github.com>
2023-05-28 21:46:20 +0530

    Latest codes added

---
+++ A6.py
@@ -0,0 +1,20 @@
+profit = [15, 27, 10, 100, 150]
+jobs = ["j1", "j2", "j3", "j4", "j5"]
+deadline = [2, 3, 3, 3, 4]
+profitNJobs = sorted(zip(profit, jobs, deadline), key=lambda x: x[0], reverse=True)
+# print (profitNJobs)
+slot = [0] * len(jobs)
+profit = 0
+ans = ['null'] * len(jobs)
+
+for i in range(len(jobs)):
+    job = profitNJobs[i]
+    for j in range(job[2], 0, -1):
+        if slot[j] == 0:
+            ans[j] = job[1]
+            profit += job[0]
+            slot[j] = 1
+            break
+
+print("J'obs scheduled':", ans[1:])
+print(profit)
